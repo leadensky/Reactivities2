11275ca5ec778b69b9270f7b1952f3950502df0c
leadensky <ben@leadensky.com>
2021-03-24 17:39:23 -0600

    Section 10, 104

--- client-app/src/app/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityListItem.tsx
@@ -14,7 +14,7 @@ export default function ActivityListItem({ activity }: Props) {
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" circular source="/assets/user.png" />
+            <Item.Image size="tiny" circular src="/assets/user.png" />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
